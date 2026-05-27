d38cf70a979b5e8f3f7e635cb2245b9e59ec16d5
Dirushan <143399530+shandiru@users.noreply.github.com>
2026-05-27 21:37:44 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = ({ overlay = false }) => {
             <img
               src={logoUrl}
               alt={branding.logoAlt}
-              className="h-11 w-auto object-contain sm:h-12 lg:h-14"
+              className="h-11 w-auto object-contain sm:h-12 lg:h-18"
             />
           )}
         </HashLink>
